356bdaeaef14eae1d0dc76284551f8ffc71e4a4c
raylrnd <382526825@qq.com>
2020-04-13 12:41:29 +0800

    init Spring Boot Project

---
+++ src/main/java/com/example/tinyrpc/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.tinyrpc;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/com/example/tinyrpc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinyrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
