d31f533e2660439335ccc9bfe09c9ede9659903c
raylrnd <382526825@qq.com>
2020-05-19 09:42:46 +0800

    同步项目

--- src/main/java/com/example/tinyrpc/Codec/MyDecoder.java
+++ src/main/java/com/example/tinyrpc/Codec/MyDecoder.java
@@ -3,6 +3,7 @@ package com.example.tinyrpc.Codec;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import sun.jvm.hotspot.runtime.Bytes;
 
 import java.util.List;
 
@@ -10,30 +11,54 @@ import java.util.List;
  * @auther zhongshunchao
  * @date 08/05/2020 08:23
  */
+
+/**
+ * 自定义报文格式
+ *
+ */
 public class MyDecoder extends ByteToMessageDecoder {
+    // message flag.
+    protected static final byte FLAG_REQUEST = (byte) 0x80;
+    protected static final byte FLAG_EVENT = (byte) 0x20;
+    // 低5位
+    protected static final int SERIALIZATION_MASK = 0x1f;
 
-    private Class<?> genericClass;
-
-    public MyDecoder(Class<?> genericClass) {
-        this.genericClass = genericClass;
-    }
-
-    //先读头部，解析出消息的长度
+    // 先读头部，解析出消息的长度
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        if (in.readableBytes() < 4) {
-            return;
+    protected void decode(ChannelHandlerContext ctx, ByteBuf buffer, List<Object> out) {
+        if (buffer.readableBytes() < 10) {
+            // 头部异常
         }
-        in.markReaderIndex();
-        int dataLength = in.readInt();
-        if (in.readableBytes() < dataLength) {
-            in.resetReaderIndex();
-            return;
-        }
-        byte[] data = new byte[dataLength];
-        in.readBytes(data);
+        // 解析头部
+        byte [] header = new byte[10];
+        buffer.readBytes(header);
+        byte flag = header[0];
+        // 采用的序列化协议
+        byte serializationProto = (byte) (flag & SERIALIZATION_MASK);
+        // get request id.
+        long requestId = BytesUtil.bytes2long(header, 4);
+        // 开始body解析
+        // 为响应报文
+        if ((flag & FLAG_REQUEST) == 0) {
+            // decode response.
+            Response res = new Response(requestId);
+            if ((flag & FLAG_EVENT) != 0) {
+                res.setEvent(true);
+            }
+            // get status.
+            byte status = header[1];
+            res.setStatus(status);
+            if (status == Response.OK) {
+                Object data;
+                // 为心跳事件，如果是PING，直接返回PONG
+                if (res.isEvent()) {
+                    //
+                }
+            }
+        } else {
+            // 解析请求报文
 
-        Object obj = SerializationUtil.deserializer(data, genericClass);
-        out.add(obj);
+        }
+
     }
 }
--- src/main/java/com/example/tinyrpc/Server/Server.java
+++ src/main/java/com/example/tinyrpc/Server/Server.java
@@ -1,5 +1,7 @@
 package com.example.tinyrpc.Server;
 
+import com.example.tinyrpc.Codec.MyDecoder;
+import com.example.tinyrpc.Constant.ProtocolConstant;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -7,6 +9,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -28,7 +31,9 @@ public class Server {
                             //插入到ChannelHandlerContext这个双链表当中
                             ch.pipeline().addLast(new StringDecoder())
                                     .addLast(new StringEncoder())
-                                    .addLast(new ServerHandler());
+                                    .addLast(new ServerHandler())
+                                    .addLast(new DelimiterBasedFrameDecoder(ProtocolConstant.MAX_LENGTH, ProtocolConstant.MAGIC))
+                                    .addLast(new MyDecoder());
                         }
                     }).option(ChannelOption.SO_BACKLOG, 1024);
             ChannelFuture future = bootstrap.bind(hostName, port).sync();
